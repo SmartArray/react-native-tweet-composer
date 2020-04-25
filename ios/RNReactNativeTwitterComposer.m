94c740af3b9810c3156cc167b0251871bb97181b
Yoshi <yoshi@jaeger.berlin>
2020-04-25 06:16:58 +0200

    Update

--- ios/RNReactNativeTwitterComposer.m
+++ ios/RNReactNativeTwitterComposer.m
@@ -1,4 +1,5 @@
 
+#import <TwitterKit/TWTRKit.h>
 #import "RNReactNativeTwitterComposer.h"
 
 @implementation RNReactNativeTwitterComposer
@@ -8,6 +9,47 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(init: (NSString *)consumerKey consumerSecret:(NSString *)consumerSecret)
+{
+    [[Twitter sharedInstance] startWithConsumerKey:consumerKey consumerSecret:consumerSecret];
+    return 0;
+}
+
+RCT_EXPORT_METHOD(logIn: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    [[Twitter sharedInstance] logInWithCompletion:^(TWTRSession * _Nullable sessionData, NSError * _Nullable error) {
+        if (sessionData) {
+            TWTRAPIClient *client = [TWTRAPIClient clientWithCurrentUser];
+
+            [client requestEmailForCurrentUser:^(NSString *email, NSError *error) {
+                NSString *requestedEmail = (email) ? email : @"";
+                NSDictionary *body = @{@"authToken": sessionData.authToken,
+                                       @"userID":sessionData.userID,
+                                       @"email": requestedEmail,
+                                       @"userName":sessionData.userName,
+                                       @"authTokenSecret": sessionData.authTokenSecret};
+                resolve(body);
+            }];
+        } else {
+            reject(@"Error", @"Twitter signin error", error);
+        }
+    }];
+}
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(createTweet: (void*)resolve)
+{
+    TWTRComposer *composer = [[TWTRComposer alloc] init];
+    return composer;
+}
+
+RCT_EXPORT_METHOD(logOut)
+{
+    TWTRSessionStore *store = [[Twitter sharedInstance] sessionStore];
+    NSString *userID = store.session.userID;
+    [store logOutUserID:userID];
+}
 
 @end
   
